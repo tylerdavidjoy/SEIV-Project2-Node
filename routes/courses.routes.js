e3eab0e7a9ee25c69ad4de3858f3bea71813eabd
Garrett Wagner <Garrett.Wagner@eagles.oc.edu>
2020-10-11 17:57:57 -0500

    Got GetAll working for course_prereq

--- routes/courses.routes.js
+++ routes/courses.routes.js
@@ -1,11 +1,18 @@
 module.exports = app => {
     const courses = require("../controllers/courses.controller.js");
+    const prereq = require("../controllers/prereq.controller.js");
   
     // Create a new Course
     app.post("/courses", courses.create);
+
+    // Create a new Prerequite
+    app.post("/prereq", prereq.create);//working on
   
     // Retrieve all Courses
     app.get("/courses", courses.findAll);
+
+    // Retrieve all Courses
+    app.get("/prereq", prereq.findAll);
   
     // Retrieve a single Course with courseId
     app.get("/courses/id=:courseId", courses.findOne);
